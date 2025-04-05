5304b0abda3d10997dea645ac55c2c9438655b2f
miw1702 <mew61819@gmail.com>
2025-04-05 14:42:01 +0700

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import Route from "../components/Route";
+
+function Home(){
+    return(
+        <Route>
+        <div className="p-6 text-3xl h-screen"> hello Home</div>
+        </Route>
+    );
+}
+export default Home;
